ab390f2592358892ceaf8c4c07e949725deb56ad
Richard Donald <richard_donald92@outlook.com>
2021-06-27 00:48:58 -0400

    Use AnotherClass features

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from someClass.someClass import SomeClass
-
+from anotherClass.anotherClass import AnotherClass
 x = SomeClass('joe', 20)
 
 x.doSomething(10)
+
+nums = AnotherClass(10,3,4,3,89,100,70)
+
+print('the largest number is: ', nums.find_max())
+print('the smallest number is: ', nums.find_min())
+print('all the numbers are: ', nums)
